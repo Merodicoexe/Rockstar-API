e173b8c4e0c4388becdf47603182965f5ff9b609
Merodicoexe <hitherl@seznam.cz>
2025-10-17 00:48:26 +0200

    Update api.py

--- api.py
+++ api.py
@@ -111,4 +111,5 @@ def get_codes():
         return jsonify({"error": "internal server error", "detail": str(exc)}), 500
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=4000, debug=True)
+
